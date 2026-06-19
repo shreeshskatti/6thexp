2ad095037eed28de222aeb5464f6c9e8af674a52
shreeshskatti <shreeshkatti212@gmail.com>
2026-06-19 08:07:00 +0530

    initial commit

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,20 +1,16 @@
-import org.junit.jupiter.api.Test;
+package org.example;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.Test;
 
 public class GoogleTest {
-
     @Test
     public void openWebsite() throws InterruptedException {
-
         WebDriver driver = new ChromeDriver();
-
         driver.get("https://www.google.com");
-
         System.out.println("Title: " + driver.getTitle());
-
         Thread.sleep(5000);
-
         driver.quit();
     }
 }
